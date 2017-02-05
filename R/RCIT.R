77e4bb5b72df42ffa6dcd06dab5441cd74623fd0
Eric V. Strobl <ericvonstrobl@gmail.com>
2017-02-05 13:51:48 -0500

    Add files via upload

---
+++ R/RCIT.R
@@ -0,0 +1,110 @@
+#' RCIT and RCoT - tests whether x and y are conditionally independent given z. Calls RIT if z is empty.
+#' @param x Random variable x.
+#' @param y Random variable y.
+#' @param z Random variable z.
+#' @param approx Method for approximating the null distribution. Default is the "lpd4," the Lindsay-Pilla-Basak method. Other options include "gamma" for the Satterthwaite-Welch method and "hbe" for the Hall-Buckley-Eagleson method.
+#' @param corr FALSE corresponds to RCIT and TRUE to RCoT. Default is FALSE.
+#' @param seed The seed for controlling random number generation. Use if you want to replicate results exactly. Default is NULL.
+#' @return A list containing the p-value \code{p} and statistic \code{Sta}
+#' @examples
+#' RCIT(rnorm(1000),rnorm(1000),rnorm(1000));
+#'
+#' x=rnorm(10000);
+#' y=(x+rnorm(10000))^2;
+#' z=rnorm(10000);
+#' RCIT(x,y,z,seed=2);
+
+
+RCIT <- function(x,y,z=NULL,approx="lpd4",corr=FALSE,seed=NULL){
+
+  if (length(z)==0){
+    out=RIT(x,y,approx="lpd4",seed=seed);
+    return(out)
+  }
+  else{
+
+    x=matrix2(x);
+    y=matrix2(y);
+    z=matrix2(z);
+
+    d=ncol(z);
+    if (corr){ y = cbind(y,z)};
+
+    z=z[,apply(z,2,sd)>0];
+    z=matrix2(z);
+
+    if (length(z)==0){
+      out=RIT(x,y,approx="lpd4", seed=seed);
+      return(out);
+    } else if (sd(x)==0 | sd(y)==0){
+      out=list(p=1,Sta=0);
+      return(out)
+    }
+
+    r=nrow(x);
+    if (r>500){
+      r1=500
+    } else {r1=r;}
+
+    x=normalize(x);
+    y=normalize(y);
+    z=normalize(z);
+
+
+    four_z = random_fourier_features(z[,1:d],num_f=25,sigma=median(c(t(dist(z[1:r1,])))), seed = seed );
+    four_x = random_fourier_features(x,num_f=5,sigma=median(c(t(dist(x[1:r1,])))), seed = seed );
+    four_y = random_fourier_features(y,num_f=5,sigma=median(c(t(dist(y[1:r1,])))), seed = seed );
+
+    f_x=normalize(four_x$feat);
+    f_y=normalize(four_y$feat);
+    f_z=normalize(four_z$feat);
+
+    Cxy=cov(f_x,f_y);
+
+    Czz = cov(f_z);
+
+    i_Czz = ginv(Czz); #requires library(MASS)
+    Cxz=cov(f_x,f_z);
+    Czy=cov(f_z,f_y);
+
+    Cxy_z=Cxy-Cxz%*%i_Czz%*%Czy;
+    z_i_Czz=f_z%*%i_Czz;
+    e_x_z = z_i_Czz%*%t(Cxz);
+    e_y_z = z_i_Czz%*%Czy;
+
+
+    Sta = r*sum(Cxy_z^2);
+
+    #approximate null distributions
+
+    res_x = f_x-e_x_z;
+    res_y = f_y-e_y_z;
+
+    d =expand.grid(1:ncol(f_x),1:ncol(f_y));
+    res = res_x[,d[,1]]*res_y[,d[,2]];
+    Cov = 1/r * (t(res)%*%res);
+    eig_d = eigen(Cov);
+    eig_d$values=eig_d$values[eig_d$values>0];
+
+    if (approx == "gamma"){
+      p=1-sw(eig_d$values,Sta);
+
+    } else if (approx == "hbe") {
+
+      p=1-hbe(eig_d$values,Sta);
+
+    } else if (approx == "lpd4"){
+      eig_d_values=eig_d$values;
+      p=try(1-lpb4(eig_d_values,Sta),silent=TRUE);
+      if (!is.numeric(p)){
+        p=1-hbe(eig_d$values,Sta);
+      }
+    }
+
+    if (p<0) p=0;
+
+    out=list(p=p,Sta=Sta);
+    return(out)
+  }
+
+}
