99072f26d12ae7e09a2092339ae1ed16f6cd6b79
Jakob Runge <jakobrunge@dyn235-203.bc.ic.ac.uk>
2017-05-15 16:45:13 +0100

    updated

--- tigramite/setup_ace_cython_code.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup
-from Cython.Build import cythonize
-import numpy
-
-setup(
-    ext_modules = cythonize("tigramite_cython_code.pyx"),
-    include_dirs=[numpy.get_include()]
-)
