7653ceddd7685daa151f148c48922abbee9a44f2
Jakob Runge <jakobrunge@dyn235-203.bc.ic.ac.uk>
2017-05-17 17:27:41 +0100

    updated

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ else:
         Extension("tigramite.tigramite_cython_code", 
                   ["tigramite/tigramite_cython_code.c"],
                   include_dirs=[numpy.get_include()]),
-    ],
+    ]
 
 setup(
     name='tigramite',
