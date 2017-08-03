7b8ced29f12df1d867c6414d02a0bf595a458f9b
Ewen Gillies <elg112@ic.ac.uk>
2017-08-03 17:48:08 +0100

    Trying new installation without calling pip first

--- setup.py
+++ setup.py
@@ -1,26 +1,32 @@
-# from distutils.core import setup, Extension
-from setuptools import setup, Extension
-
-import numpy
-
 import io
+import os
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
 
-# If cython is available, the included cython *.pyx file
-# is compiled, otherwise the *.c file is used
-try:
-    from Cython.Distutils import build_ext
-except ImportError:
-    use_cython = False
-else:
-    use_cython = True
+# Handle building against numpy headers before installing numpy
+class UseNumpyHeadersBuildExt(build_ext):
+    """
+    Subclassed build_ext command.
+    Allows for numpy to be imported after it is automatically installed.
+    This lets us use numpy.get_include() while listing numpy as a needed
+    dependency.
+    """
+    def run(self):
+        # Import numpy here, only when headers are needed
+        import numpy
+        # Add numpy headers to include_dirs
+        self.include_dirs.append(numpy.get_include())
+        # Call original build_ext command
+        build_ext.run(self)
+CMDCLASS = {'build_ext': UseNumpyHeadersBuildExt}
 
+# Read in all the installation requirements from the requirements.txt file
+PWD = os.path.abspath(os.path.dirname(__file__))
+with io.open(os.path.join(PWD, 'requirements.txt')) as f:
+    INSTALL_REQUIRES = f.read().split('\n')
 
-install_requires = [
-    'numpy',
-    'scipy'
-]
-
-extras_require = {
+# Define the extras needed
+EXTRAS_REQUIRE = {
     'Gaussian Process (GP) Regression':  ['sklearn>=0.18'],
     'R-based ACE, also requires acepack installed in R':  ['rpy2'],
     'pure-python ACE':  ['ace>=0.3'],
@@ -28,48 +34,37 @@ extras_require = {
     'p-value corrections': ['statsmodels']
 }
 
-tests_require = ['nose']
+# Define the packages needed for testing
+TESTS_REQUIRE = ['nose']
 
-
-cmdclass = { }
-ext_modules = [ ]
-
-if use_cython:
-    ext_modules += [
-        Extension("tigramite.tigramite_cython_code", 
-                  [ "tigramite/tigramite_cython_code.pyx" ],
-                  include_dirs=[numpy.get_include()]),
-    ]
-    cmdclass.update({ 'build_ext': build_ext })
-else:
-    ext_modules += [
-        Extension("tigramite.tigramite_cython_code", 
-                  ["tigramite/tigramite_cython_code.c"],
-                  include_dirs=[numpy.get_include()]),
-    ]
+# Define the external modules to build
+EXT_MODULES = [Extension("tigramite.tigramite_cython_code",
+                         ["tigramite/tigramite_cython_code.c"])]
 
 setup(
     name='tigramite',
     version='3.0b0',
-    packages=['tigramite',],
+    packages=['tigramite'],
     license='GNU General Public License v3.0',
     description='Tigramite causal discovery for time series',
     author='Jakob Runge',
     author_email='jakobrunge@posteo.de',
     url='https://github.com/jakobrunge/tigramite/',
     long_description=io.open('README.md', 'r', encoding='utf-8').read(),
-    keywords = 'causality time-series',
-    cmdclass = cmdclass,
-    ext_modules=ext_modules,
-    install_requires=install_requires,
-    test_suite = 'nose.collector',
-    tests_require = tests_require,
+    keywords='causality time-series',
+    cmdclass=CMDCLASS,
+    ext_modules=EXT_MODULES,
+    install_requires=INSTALL_REQUIRES,
+    test_suite='nose.collector',
+    tests_require=TESTS_REQUIRE,
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Science/Research',
         'Topic :: Scientific/Engineering :: Artificial Intelligence',
         'Topic :: Scientific/Engineering :: Mathematics',
-        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'License '\
+            ':: OSI Approved '\
+            ':: GNU General Public License v3 or later (GPLv3+)',
         'Programming Language :: Python :: 2.7',
     ],
 )
