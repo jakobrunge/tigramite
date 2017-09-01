d89d1f9effba86b1f81d9e4965206147938acd0e
Ewen Gillies <elg112@ic.ac.uk>
2017-09-01 14:20:43 +0100

    Full parameterized fixtures and tests working

--- tests/test_tigramite_pcmci.py
+++ tests/test_tigramite_pcmci.py
@@ -8,6 +8,10 @@ from tigramite.pcmci import PCMCI
 from tigramite.independence_tests import ParCorr #, GPACE
 import tigramite.data_processing as pp
 
+# Define the verbosity at the global scope
+VERBOSITY = 0
+
+# CONVENIENCE FUNCTIONS ########################################################
 def assert_graphs_equal(actual, expected):
     """
     Check whether lists in dict have the same elements.
@@ -33,12 +37,21 @@ def _select_links(link_ids, true_parents):
     """
     if link_ids is None:
         return None
-    return {par : [true_parents[par][link]] for par in true_parents\
+    return {par : [true_parents[par][link]] for par in true_parents \
                                             for link in link_ids}
 
-# Define the verbosity at the global scope
-VERBOSITY = 0
+def _get_parents_from_results(pcmci, results, alpha_level):
+    """
+    Select the significant parents from the MCI-like results at a given
+    alpha_level
+    """
+    significant_parents = \
+        pcmci._return_significant_parents(pq_matrix=results['p_matrix'],
+                                          val_matrix=results['val_matrix'],
+                                          alpha_level=alpha_level)
+    return significant_parents['parents']
 
+# TEST DATA GENERATION #########################################################
 @pytest.fixture(params=[
     # Generate a test data sample
     # Parameterize the sample by setting the autocorrelation value, coefficient
@@ -62,12 +75,7 @@ def a_sample(request):
     true_parents = _get_parent_graph(links_coeffs)
     return pp.DataFrame(data), true_parents
 
-@pytest.fixture()
-    # Parameterize and return the independence test.
-    # Currently just a warpper for ParCorr, but is extentable
-def a_test(request):
-    return ParCorr(verbosity=VERBOSITY)
-
+# PCMCI CONSTRUCTION ###########################################################
 @pytest.fixture(params=[
     # Keep parameters common for all the run_ algorithms here
     # tau_min, tau_max,  sel_link,
@@ -75,8 +83,13 @@ def a_test(request):
      (1,       2,        [0])])
 def a_common_params(request):
     # Unpack the parameters to return them in a common tuple
-    tau_min, tau_max, sel_link = request.param
-    return tau_min, tau_max, sel_link
+    return request.param
+
+@pytest.fixture()
+    # Parameterize and return the independence test.
+    # Currently just a wrapper for ParCorr, but is extendable
+def a_test(request):
+    return ParCorr(verbosity=VERBOSITY)
 
 @pytest.fixture(params=[None, [1]])
     # Fixture to build and return a parameterized PCMCI.  Different selected
@@ -101,77 +114,95 @@ def a_pcmci(a_sample, a_test, a_common_params, request):
     # Ensure we change the true parents to be the same as the selected links
     if select_links is not None:
         true_parents = select_links
+    # Return the constructed PCMCI, expected results, and common parameters
     return pcmci, true_parents, tau_min, tau_max, select_links
 
-def test_pcmci(a_pcmci):
-    return True
-    # Unpack the pcmci and the true parents
-    pcmci, true_parents, tau_min, tau_max, select_links = a_pcmci
-
-    results = pcmci.run_pcmci(
-        selected_links=select_links,
-        tau_min=tau_min,
-        tau_max=tau_max,
-        pc_alpha=pc_alpha,
-    )
-
-    parents = pcmci._return_significant_parents(
-        pq_matrix=results['p_matrix'],
-        val_matrix=results['val_matrix'],
-        alpha_level=alpha_level)['parents']
-
-    assert_graphs_equal(parents, true_parents)
-
+# PC_STABLE TESTING ############################################################
 @pytest.fixture(params=[
     # Keep parameters for the pc_stable algorithm here
-    # pc_alpha,  max_cond_dim,  max_comb 
+    # pc_alpha,  max_conds_dim,  max_comb
      (0.05,      None,          1),
      (0.05,      2,             1)])
 def a_pc_stable_params(request):
     # Return the parameters for the pc_stable test
-    pc_alpha, max_cond_dim, max_comb = request.param
-    return pc_alpha, max_cond_dim, max_comb
+    return request.param
 
 @pytest.fixture()
 def a_run_pc_stable(a_pcmci, a_pc_stable_params):
     # Unpack the pcmci, true parents, and common parameters
     pcmci, true_parents, tau_min, tau_max, select_links = a_pcmci
     # Unpack the pc_stable parameters
-    pc_alpha, max_cond_dim, max_combinations = a_pc_stable_params
+    pc_alpha, max_conds_dim, max_combinations = a_pc_stable_params
     # Run PC stable
     pcmci.run_pc_stable(selected_links=select_links,
                         tau_min=tau_min,
                         tau_max=tau_max,
-                        save_iterations=False,
                         pc_alpha=pc_alpha,
-                        max_conds_dim=max_cond_dim,
+                        max_conds_dim=max_conds_dim,
                         max_combinations=max_combinations)
+    # Return the calculated and expected results
     return pcmci.all_parents, true_parents
 
 def test_pc_stable(a_run_pc_stable):
-    # Unpack the caclulated and true parents
+    # Unpack the calculated and true parents
     parents, true_parents = a_run_pc_stable
     # Ensure they are the same
     assert_graphs_equal(parents, true_parents)
 
-def test_mci(a_pcmci):
-    return True
-    # Unpack the pcmci and the true parents
-    pcmci, true_parents, tau_min, tau_max, select_links = a_pcmci
+# MCI TESTING ##################################################################
+@pytest.fixture(params=[
+    # Keep parameters for the mci algorithm here
+    # alpha_level, max_conds_px, max_conds_py
+     (0.01,        None,         None)])
+def a_mci_params(request):
+    # Return the parameters for the mci test
+    return request.param
 
+@pytest.fixture()
+def a_run_mci(a_pcmci, a_mci_params):
+    # Unpack the pcmci and the true parents, and common parameters
+    pcmci, true_parents, tau_min, tau_max, select_links = a_pcmci
+    # Unpack the MCI parameters
+    alpha_level, max_conds_px, max_conds_py = a_mci_params
+    # Run the MCI algorithm with the given parameters
     results = pcmci.run_mci(selected_links=select_links,
-                            tau_min=1,
+                            tau_min=tau_min,
                             tau_max=tau_max,
                             parents=true_parents,
-                            max_conds_py=None,
-                            max_conds_px=None)
+                            max_conds_py=max_conds_px,
+                            max_conds_px=max_conds_py)
+    # Return the calculated and expected results
+    return _get_parents_from_results(pcmci, results, alpha_level), true_parents
 
-    parents = pcmci._return_significant_parents(
-        pq_matrix=results['p_matrix'],
-        val_matrix=results['val_matrix'],
-        alpha_level=alpha_level)['parents']
-
+def test_mci(a_run_mci):
+    # Unpack the calculated and true parents
+    parents, true_parents = a_run_mci
+    # Ensure they are the same
     assert_graphs_equal(parents, true_parents)
 
-#alpha level 0.01
-#pc_alpha 0.05
+# PCMCI TESTING ################################################################
+@pytest.fixture()
+def a_run_pcmci(a_pcmci, a_pc_stable_params, a_mci_params):
+    # Unpack the pcmci and the true parents, and common parameters
+    pcmci, true_parents, tau_min, tau_max, select_links = a_pcmci
+    # Unpack the pc_stable parameters
+    pc_alpha, max_conds_dim, max_combinations = a_pc_stable_params
+    # Unpack the MCI parameters
+    alpha_level, max_conds_px, max_conds_py = a_mci_params
+    # Run the PCMCI algorithm with the given parameters
+    results = pcmci.run_pcmci(selected_links=select_links,
+                              tau_min=tau_min,
+                              tau_max=tau_max,
+                              pc_alpha=pc_alpha,
+                              max_conds_dim=max_conds_dim,
+                              max_combinations=max_combinations,
+                              max_conds_px=max_conds_px,
+                              max_conds_py=max_conds_py)
+    # Return the results and the expected result
+    return _get_parents_from_results(pcmci, results, alpha_level), true_parents
+
+def test_pcmci(a_run_pcmci):
+    # Unpack the calculated and true parents
+    parents, true_parents = a_run_pcmci
+    # Ensure they are the same
+    assert_graphs_equal(parents, true_parents)
