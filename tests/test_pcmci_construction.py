c17316d5b3890fadd1ff2d737ca90c2de3578594
Ewen Gillies <elg112@ic.ac.uk>
2017-10-07 21:07:57 +0100

    added test doc strings, added test for cond_iterator

--- tests/test_pcmci_construction.py
+++ tests/test_pcmci_construction.py
@@ -5,6 +5,7 @@ from __future__ import print_function
 from collections import defaultdict
 import numpy as np
 import pytest
+from scipy.misc import comb
 
 from tigramite.pcmci import PCMCI
 from tigramite.independence_tests import ParCorr #, GPACE
@@ -75,21 +76,31 @@ def a_pcmci(a_sample, request):
 
 # TEST VARIABLE SELECTION ######################################################
 def get_select_vars(some_vars):
-    #Yield some subset of the input vars
+    """
+    Yield some subsets of the input variables
+    """
     for subset in [some_vars[0::2], some_vars[1::2]]:
         yield subset
 
 def test_select_vars_default(a_pcmci):
+    """
+    Check that _set_selected_variables returns all variables by default.
+    """
     # Unpack the pcmci instance
     pcmci, _ = a_pcmci
     # Test the default selected variables are correct
     all_vars = pcmci._set_selected_variables(None)
-    err_msg = "Default option for _select_variables should return all variables"
+    err_msg = "Default option for _set_selected_variables should return all"+\
+              " variables"
     np.testing.assert_array_equal(list(all_vars),
                                   list(range(pcmci.N)),
                                   err_msg=err_msg)
 
 def test_select_vars_setting(a_pcmci):
+    """
+    Check that _set_selected_variables returns the correct subset of all variables
+    when requested.
+    """
     # Unpack the pcmci instance
     pcmci, _ = a_pcmci
     # Test that both out of range and negative numbers throw an error while
@@ -104,6 +115,10 @@ def test_select_vars_setting(a_pcmci):
                                       err_msg=err_msg)
 
 def test_select_vars_errors(a_pcmci):
+    """
+    Check that _set_selected_variables throws the correct errors for bad requests
+    but does not crash for good requests.
+    """
     # Unpack the pcmci instance
     pcmci, _ = a_pcmci
     # Test that both out of range and negative numbers throw an error while
@@ -127,7 +142,7 @@ def test_select_vars_errors(a_pcmci):
 
 # TEST LINK SELECTION ##########################################################
 TAU_MIN = 1
-TAU_MAX = 5
+TAU_MAX = 3
 def get_expected_links(a_range, b_range, t_range):
     """
     Helper function to generate the expected links
@@ -136,6 +151,9 @@ def get_expected_links(a_range, b_range, t_range):
             for a_var in a_range}
 
 def test_select_links_default(a_pcmci):
+    """
+    Check that _set_sel_links returns all links by default.
+    """
     # Unpack the pcmci instance
     pcmci, _ = a_pcmci
     # Test the default selected variables are correct
@@ -148,6 +166,9 @@ def test_select_links_default(a_pcmci):
     np.testing.assert_equal(sel_links, good_links, err_msg=err_msg)
 
 def test_select_links_setting(a_pcmci):
+    """
+    Check that _set_sel_links returns only links of selected variables.
+    """
     # Unpack the pcmci instance
     pcmci, _ = a_pcmci
     # Seleect some of the variables
@@ -167,8 +188,13 @@ def test_select_links_setting(a_pcmci):
         np.testing.assert_equal(sel_links, good_links, err_msg=err_msg)
 
 def test_select_links_errors(a_pcmci):
+    """
+    Check that _set_sel_links throws the correct errors when out of range
+    variables are mentioned.
+    """
+    # Unpack the pcmci instance
     pcmci, _ = a_pcmci
-    # Seleect some of the variables
+    # Select some of the variables
     for sel_vars in get_select_vars(range(pcmci.N)):
         # Initialize the list
         pcmci.selected_variables = pcmci._set_selected_variables(sel_vars)
@@ -188,3 +214,49 @@ def test_select_links_errors(a_pcmci):
             with pytest.raises(ValueError, message=err_msg):
                 test_links[bad_val] = [(bad_val, TAU_MAX)]
                 _ = pcmci._set_sel_links(test_links, TAU_MIN, TAU_MAX)
+
+# TEST ITERATORS ###############################################################
+def test_condition_iterator(a_pcmci):
+    """
+    Test if the correct conditions are returned from the iterator.
+    """
+    # Unpack the pcmci algorithm
+    pcmci, _ = a_pcmci
+    # Get all possible links
+    sel_links = pcmci._set_sel_links(None, TAU_MIN, TAU_MAX)
+    # Loop over all possible condition dimentions
+    max_cond_dim = pcmci._set_max_condition_dim(None, TAU_MAX)
+    # Loop over all nodes
+    for j in range(pcmci.N):
+        # Initialise the list of conditions for this node
+        node_conds = list()
+        expected_n_conds = 0
+        # Parents for this node
+        parents = sel_links[j]
+        # Loop over all possible dimentionality of conditions
+        # TODO ask jakob about max_conds_dim in source and maybe change this 
+        # limit to max_cond_dim + 1
+        for cond_dim in range(max_cond_dim):
+            # Iterate through all possible pairs (that have not converged yet)
+            for par in parents:
+                # Number of conditions for this dimension
+                n_conds = comb(len(parents) - 1, cond_dim, exact=True)
+                expected_n_conds += n_conds
+                # Create an array for these conditions
+                par_conds = list()
+                # Iterate through all possible combinations
+                for cond in pcmci._iter_conditions(par, j, cond_dim, parents):
+                    # Ensure the condition does not contain the parent
+                    assert par not in cond, "Parent should not be in condition"
+                    # Keep this condition in list of conditions
+                    node_conds += [cond]
+                    par_conds.append([par] + cond)
+                    assert len(cond) == cond_dim,\
+                        "Number of conditions should match requested dimension"
+                # Check that all condition, parent pairs are unique
+                par_conds = np.array(par_conds)
+                print(cond_dim, expected_n_conds)
+                assert np.unique(par_conds, axis=0).shape == par_conds.shape
+        # Check that the right number of conditions are found
+        assert len(node_conds) == expected_n_conds, \
+            "Expected number of combinations found"
