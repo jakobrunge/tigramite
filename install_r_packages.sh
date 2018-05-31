02cc7115c51d277c335d0ecedfd6e93a4a8042b5
EwenGillies <elg112@ic.ac.uk>
2018-05-31 15:31:44 +0100

    added default mirrors for R

--- install_r_packages.sh
+++ install_r_packages.sh
@@ -1,10 +1,10 @@
 # Install rpy2 and some dependencies
 conda install rpy2
 # Install r-MASS package, which is a dependency of RCIT/RCOT
-R -e 'install.packages("MASS")' 
+R -e 'install.packages("MASS", repos = "https://cloud.r-project.org/")' 
 # Install momentchi2, whichs is a dependency of RCIT/RCOT
-R -e 'install.packages("momentchi2")' 
+R -e 'install.packages("momentchi2", repos = "https://cloud.r-project.org/")' 
 # Install devtools so we can install from github
-R -e 'install.packages("devtools")'
+R -e 'install.packages("devtools", repos = "https://cloud.r-project.org/")' 
 # Install RCOT/RCIT
 R -e 'library(devtools); devtools::install_github("ericstrobl/RCIT")'
