44f9d6425df9cd008e0953a2d169d2231434170f
EwenGillies <elg112@ic.ac.uk>
2018-08-04 19:09:21 +0100

    updated build tools

--- build_tools/conda/build.sh
+++ build_tools/conda/build.sh
@@ -1,14 +1,13 @@
+# Install r-MASS package, which is a dependency of RCIT/RCOT
+R -e "install.packages('MASS', '$PREFIX/lib/R/library', repos='https://cloud.r-project.org/')"
+# Install momentchi2, whichs is a dependency of RCIT/RCOT
+R -e "install.packages('momentchi2', '$PREFIX/lib/R/library', repos = 'https://cloud.r-project.org/')"
+# Install the RCIT package
+R -e "install.packages('external_packages/RCIT', '$PREFIX/lib/R/library', repos=NULL, type='source')"
+# Install tigramite
+$PYTHON setup.py install --single-version-externally-managed --record=record.txt
 echo EWEN DEBUG
 echo $PREFIX
 ls $PREFIX
 pwd
 ls
-return 1
-# Install r-MASS package, which is a dependency of RCIT/RCOT
-R -e "install.packages('MASS', '$PREFIX/lib/R/library/', repos='https://cloud.r-project.org/')"
-# Install momentchi2, whichs is a dependency of RCIT/RCOT
-R -e "install.packages('momentchi2', '$PREFIX/lib/R/library/', repos = 'https://cloud.r-project.org/')"
-# Install the RCIT package
-#R -e install.packages(path_to_file, repos = NULL, type="source")
-# Install tigramite
-$PYTHON setup.py install --single-version-externally-managed --record=record.txt
