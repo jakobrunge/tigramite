27ed138a61a5d9ae2398e2cc9d708f76f3b05074
EwenGillies <elg112@ic.ac.uk>
2018-08-04 18:57:56 +0100

    updated build tools

--- build_tools/conda/build.sh
+++ build_tools/conda/build.sh
@@ -1,6 +1,14 @@
-# Install the r-packages
-./install_r_packages.sh
+echo EWEN DEBUG
+echo $PREFIX
+ls $PREFIX
+return 1
+# Install r-MASS package, which is a dependency of RCIT/RCOT
+R -e 'install.packages("MASS", repos = "https://cloud.r-project.org/")' 
+# Install momentchi2, whichs is a dependency of RCIT/RCOT
+R -e 'install.packages("momentchi2", repos = "https://cloud.r-project.org/")' 
+# Install devtools so we can install from github
+R -e 'install.packages("devtools", repos = "https://cloud.r-project.org/")' 
+# Install RCOT/RCIT
+R -e 'devtools::install("external_packages/RCIT")'
 # Install tigramite
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
-echo EWEN DEBUG
-ls $PREFIX
