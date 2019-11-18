56f140cb4573fbeab02c1f292b0fac662f8e7775
ckaeding <christoph.kaeding@dlr.de>
2019-11-18 13:19:04 +0100

    fix rcot

--- tigramite/independence_tests.py
+++ tigramite/independence_tests.py
@@ -2573,13 +2573,18 @@ class RCOT(CondIndTest):
         x_vals = array[0]
         y_vals = array[1]
         z_vals = np.fastCopyAndTranspose(array[2:])
-        rcot = np.asarray(rpy2.robjects.r['RCoT'](x_vals, y_vals, z_vals,
-                                                  num_f=self.num_f,
-                                                  approx=self.approx,
-                                                  seed=self.seed))
-        val = float(rcot[1])
+
+        rcot_ret = rpy2.robjects.r['RCoT'](x_vals, y_vals, z_vals,
+                                           num_f=self.num_f,
+                                           approx=self.approx,
+                                           seed=self.seed)
+
+        rcot = { key : rcot_ret.rx2(key)[0] for key in rcot_ret.names }
+
+        val = float(rcot['Sta'])
         # Cache the p-value for use later
-        self._pval = float(rcot[0])
+        self._pval = float(rcot['p'])       
+        
         return val
 
     def get_analytic_significance(self, **args):
