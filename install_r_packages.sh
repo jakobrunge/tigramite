9a92c09ecbee0e2e7f29a25823c5a10833d38ba8
Christoph Kaeding <christoph.kaeding@dlr.de>
2019-11-26 11:11:34 +0100

    windows stuff

--- install_r_packages.sh
+++ install_r_packages.sh
@@ -1,8 +1,8 @@
 # Install r-MASS package, which is a dependency of RCIT/RCOT
-R -e 'install.packages("MASS", repos = "https://cloud.r-project.org/")' 
+R -e "install.packages('MASS', repos = 'https://cloud.r-project.org/')"
 # Install momentchi2, whichs is a dependency of RCIT/RCOT
-R -e 'install.packages("momentchi2", repos = "https://cloud.r-project.org/")' 
+R -e "install.packages('momentchi2', repos = 'https://cloud.r-project.org/')"
 # Install devtools so we can install from github
-R -e 'install.packages("devtools", repos = "https://cloud.r-project.org/")' 
+R -e "install.packages('devtools', repos = 'https://cloud.r-project.org/')"
 # Install RCOT/RCIT
-R -e 'devtools::install("external_packages/RCIT")'
+R -e "devtools::install('external_packages/RCIT')"
