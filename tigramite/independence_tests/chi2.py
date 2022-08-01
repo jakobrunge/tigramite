b6ecea7f1e8adc2d18ebbc47b9e82b6fc42e0d56
wang70880 <wjcuhk@gmail.com>
2022-08-01 16:10:03 +0800

    [Implementation] Implement the g-sqaure test. Prepare to test it.

--- tigramite/independence_tests/chi2.py
+++ tigramite/independence_tests/chi2.py
@@ -37,74 +37,11 @@ class ChiSquare(CondIndTest):
         if self.verbosity > 0:
             print("n_symbs = %s" % self.n_symbs)
 
-    def _bincount_hist(self, symb_array, weights=None):
-        """Computes histogram from symbolic array.
-
-        The maximum of the symbolic array determines the alphabet / number
-        of bins.
-
-        Parameters
-        ----------
-        symb_array : integer array
-            Data array of shape (dim, T).
-
-        weights : float array, optional (default: None)
-            Optional weights array of shape (dim, T).
-
-        Returns
-        -------
-        hist : array
-            Histogram array of shape (base, base, base, ...)*number of
-            dimensions with Z-dimensions coming first.
-        """
-
-        if self.n_symbs is None:
-            n_symbs = int(symb_array.max() + 1)
-        else:
-            n_symbs = self.n_symbs
-            if n_symbs < int(symb_array.max() + 1):
-                raise ValueError("n_symbs must be >= symb_array.max() + 1 = {}".format(symb_array.max() + 1))
-
-        if 'int' not in str(symb_array.dtype):
-            raise ValueError("Input data must of integer type, where each "
-                             "number indexes a symbol.")
-
-        dim, T = symb_array.shape
-        """
-        JC NOTE: When the dim is high (especially because of the number of conditioning variables),
-        the computation of histogram is memory-intensive and time-intensive, even for binary variables.
-        Unfortunately, currently we can only limit the maximum number of conditioning variables (through parameter specification).
-        Future work can be explored to implement a new way to compute the histogram.
-        """
-        flathist = np.zeros((n_symbs ** dim), dtype='int16')
-        multisymb = np.zeros(T, dtype='int64')
-        if weights is not None:
-            flathist = np.zeros((n_symbs ** dim), dtype='float32')
-            multiweights = np.ones(T, dtype='float32')
-
-        for i in range(dim):
-            multisymb += symb_array[i, :] * n_symbs ** i
-            if weights is not None:
-                multiweights *= weights[i, :]
-
-        if weights is None:
-            result = np.bincount(multisymb)
-        else:
-            result = (np.bincount(multisymb, weights=multiweights)
-                      / multiweights.sum())
-
-        flathist[:len(result)] += result
-
-        hist = flathist.reshape(tuple([n_symbs, n_symbs] +
-                                      [n_symbs for i in range(dim - 2)])).T
-        print(hist.shape)
-        print(hist[1,:,:])
-        return hist
-    
     def get_dependence_measure(self, array, xyz):
-        dim, T = array.shape
+        arr = array.T
+        T, dim = arr.shape
         col_names = (list(string.ascii_lowercase) + list(string.ascii_uppercase))[:dim]
         assert(len(col_names) == dim)
-        data = pd.DataFrame(array, columns=col_names)
-        statistic_val, p_value, dof = g_sq(X=col_names[0], Y=col_names[1], Z=col_names[2:], data=data, boolean=False)
+        df = pd.DataFrame(data=arr, columns=col_names)
+        statistic_val, p_value, dof = g_sq(X=col_names[0], Y=col_names[1], Z=col_names[2:], data=df, boolean=False)
         return statistic_val, p_value
