23cfac5c9225706e260e89145e4a6f6edfe6d3f1
hoch_tm <tom.hochsprung@dlr.de>
2024-02-21 15:23:42 +0100

    additional unit tests

--- tests/test_PairwiseMultCI.py
+++ tests/test_PairwiseMultCI.py
@@ -125,37 +125,34 @@ def pairwise_mult_ci2(request):
     if sig != "fixed_thres":
         return PairwiseMultCI(learn_augmented_cond_sets = learn_augmented_cond_sets, cond_ind_test = cond_ind_test(significance = sig),
                           alpha_pre = alpha_pre,
-                          pre_step_sample_fraction = pre_step_sample_fraction)
+                          pre_step_sample_fraction = pre_step_sample_fraction), cond_ind_test(significance = sig)
     else:
         return PairwiseMultCI(learn_augmented_cond_sets = learn_augmented_cond_sets, cond_ind_test = cond_ind_test(significance = sig),
                           alpha_pre = None,
                           pre_step_sample_fraction=pre_step_sample_fraction,
                           significance= sig,
                           fixed_thres_pre = 2
-                          )
+                          ), cond_ind_test(significance = sig)
 
-def compare_ci(request):
+
+"""def compare_ci(request):
     # Unpack the parameters
     learn_augmented_cond_sets, sig, alpha_pre, pre_step_sample_fraction, cond_ind_test, cond_ind_test_thres, cond_ind_test_thres_pre = request.param
     # Generate the par_corr_wls independence test
     return cond_ind_test(significance = sig)
-
+"""
 @pytest.fixture(params=[
     # Generate PairwiseMultCI test instances
     # test PairwiseMultCI for different combinations of:
     # seed,  true_dep, T (=sample size)
     (123, 0, 100),
-    (123, 0, 1000),
     (123, 0.2, 100),
-    (123, 0.2, 1000),
     (123, 0.5, 100),
     (123, 0, 1000),
     (46, 0, 100),
-    (46, 0, 1000),
     (46, 0.2, 100),
-    (46, 0.2, 1000),
     (46, 0.5, 100),
-    (46, 0, 1000),
+    (46, 0, 100),
 ])
 
 
@@ -167,21 +164,21 @@ def data_sample_c_cc_cc2(request):
     z2 = np.random.normal(0, 1, T).reshape(T, 1)
     z = np.hstack((z1,z2))
     x = np.random.normal(0, 1, T).reshape(T, 1) + z1 + z2
-    y = np.random.normal(0, 1, T).reshape(T, 1) + z1 + z2
+    y = np.random.normal(0, 1, T).reshape(T, 1) + z1 + z2 + true_dep * x
 
 
     # Return data xyz
     return x, y, z
 
-def compare_pairwise_mult_ci(pairwise_mult_ci2, compare_ci, data_sample_c_cc_cc2):
+def test_compare_pairwise_mult_ci(pairwise_mult_ci2, data_sample_c_cc_cc2):
     # Get the data sample values
     x, y, z = data_sample_c_cc_cc2
     # Get the analytic significance
-    test_result = pairwise_mult_ci.run_test_raw(x = x, y = y, z = z, alpha_or_thres=1)
-    test_result2 = compare_ci.run_test_raw(x=x, y=y, z=z, alpha_or_thres=1)
+    test_result = pairwise_mult_ci2[0].run_test_raw(x = x, y = y, z = z, alpha_or_thres=1)
+    test_result2 = pairwise_mult_ci2[1].run_test_raw(x=x, y=y, z=z, alpha_or_thres=1)
     val = test_result[0]
     pval = test_result[1]
     val2 = test_result2[0]
     pval2 = test_result2[1]
-    np.testing.assert_allclose(val-val2, 0.0, atol=0.001)
-    np.testing.assert_allclose(pval - pval2, 0.0, atol=0.001)
+    np.testing.assert_allclose(val, val2, rtol=0.2)
+    np.testing.assert_allclose(pval, pval2, atol=0.2)
