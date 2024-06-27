5af0e247a212e7939e1537a8059bd9096ec8cd3f
jakobrunge <jakobrunge@gmail.com>
2024-06-27 13:59:18 +0200

    updated regressionCI

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ CMDCLASS = {"build_ext": UseNumpyHeadersBuildExt}
 # Run the setup
 setup(
     name="tigramite",
-    version="5.2.5.9",
+    version="5.2.5.10",
     packages=["tigramite", "tigramite.independence_tests", "tigramite.toymodels"],
     license="GNU General Public License v3.0",
     description="Tigramite causal inference for time series",
