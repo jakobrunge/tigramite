mostly reverting back to commit on 19/06/2025 (with small additional fixes), i.e., no mean block length pre-computation

tigramite/causal_effects.py
@@ -1143,10 +1143,9 @@ class CausalEffects(Graphs):
 
         return predicted_array
 
-
     def fit_bootstrap_of(self, method, method_args, 
                         boot_samples=100,
-                        boot_blocklength=1,
+                        boot_meanblocklength=1,
                         seed=None):
         """Runs chosen method on bootstrap samples drawn from DataFrame.
         
@@ -1166,8 +1165,11 @@ class CausalEffects(Graphs):
             Arguments passed to method.
         boot_samples : int
             Number of bootstrap samples to draw.
-        boot_blocklength : int, optional (default: 1)
-            Block length for block-bootstrap.
+        boot_meanblocklength : int or float, or in {'cube_root','from_autocorrelation'}
+            Mean block length for the stationary block-bootstrap. If 'cube_root' it is
+            the cube root of the time series length. If 'from_autocorrelation', the 
+            mean block length is determined from the decay of the autocorrelation
+            as described in Politis and White (2004) and Patton et al. (2009).
         seed : int, optional(default = None)
             Seed for RandomState (default_rng)
         """
@@ -1193,7 +1195,7 @@ class CausalEffects(Graphs):
             print("\n##\n## Running Bootstrap of %s " % method +
                   "\n##\n" +
                   "\nboot_samples = %s \n" % boot_samples +
-                  "\nboot_blocklength = %s \n" % boot_blocklength
+                  "\nboot_meanblocklength = %s \n" % boot_meanblocklength
                   )
 
         method_args_bootstrap = deepcopy(method_args)
@@ -1207,7 +1209,7 @@ class CausalEffects(Graphs):
             else:
                 random_state = np.random.default_rng(seed*boot_samples + b)
 
-            method_args_bootstrap['dataframe'].bootstrap = {'boot_blocklength':boot_blocklength,
+            method_args_bootstrap['dataframe'].bootstrap = {'boot_meanblocklength':boot_meanblocklength,
                                                             'random_state':random_state}
 
             # Call method and save fitted model

tigramite/pcmci_base.py
@@ -778,8 +778,9 @@ class PCMCIbase():
 
     def run_bootstrap_of(self, method, method_args,
                         boot_samples=100,
-                        boot_blocklength=1,
-                        conf_lev=0.9, seed=None):
+                        boot_meanblocklength=1,
+                        conf_lev=0.9,
+                        seed=None):
         """Runs chosen method on bootstrap samples drawn from DataFrame.
 
         Bootstraps for tau=0 are drawn from [2xtau_max, ..., T] and all lagged
@@ -809,11 +810,14 @@ class PCMCIbase():
             Arguments passed to method.
         boot_samples : int
             Number of bootstrap samples to draw.
-        boot_blocklength : int, optional (default: 1)
-            Block length for block-bootstrap.
-        conf_lev : float, optional (default: 0.9)
+        boot_meanblocklength : int or float, or in {'cube_root','from_autocorrelation'}
+            Mean block length for the stationary block-bootstrap. If 'cube_root' it is
+            the cube root of the time series length. If 'from_autocorrelation', the
+            mean block length is determined from the decay of the autocorrelation
+            as described in Politis and White (2004) and Patton et al. (2009).
+            conf_lev : float, optional (default: 0.9)
             Two-sided confidence interval for summary results.
-        seed : int, optional(default = None)
+         seed : int, optional(default = None)
             Seed for RandomState (default_rng)
 
         Returns
@@ -853,12 +857,12 @@ class PCMCIbase():
             print("\n##\n## Running Bootstrap of %s " % method +
                   "\n##\n" +
                   "\nboot_samples = %s \n" % boot_samples +
-                  "\nboot_blocklength = %s \n" % boot_blocklength
+                  "\nboot_meanblocklength = %s \n" % boot_meanblocklength
                   )
 
         # Set bootstrap attribute to be passed to dataframe
         self.dataframe.bootstrap = {}
-        self.dataframe.bootstrap['boot_blocklength'] = boot_blocklength
+        self.dataframe.bootstrap['boot_meanblocklength'] = boot_meanblocklength
 
         boot_results = {}
         #for b in range(boot_samples):
@@ -883,9 +887,9 @@ class PCMCIbase():
                 res_item = boot_res[key]
                 if type(res_item) is np.ndarray:
                     if b == 0:
-                        boot_results[key] = np.empty((boot_samples,) 
+                        boot_results[key] = np.empty((boot_samples,)
                                                      + res_item.shape,
-                                                     dtype=res_item.dtype) 
+                                                     dtype=res_item.dtype)
                     boot_results[key][b] = res_item
                 else:
                     if b == 0:
@@ -893,16 +897,16 @@ class PCMCIbase():
                     boot_results[key][b] = res_item
 
         # Generate summary results
-        summary_results = self.return_summary_results(results=boot_results, 
+        summary_results = self.return_summary_results(results=boot_results,
                                                       conf_lev=conf_lev)
 
         # Reset bootstrap to None
         self.dataframe.bootstrap = None
 
-        return {'summary_results': summary_results, 
+        return {'summary_results': summary_results,
                 'boot_results': boot_results}
 
-    def parallelized_bootstraps(self, method, method_args, boot_seed):
+   def parallelized_bootstraps(self, method, method_args, boot_seed):
         # Pass seed sequence for this boot and set it in dataframe
         # which will generate a draw with replacement
         boot_random_state = np.random.default_rng(boot_seed)