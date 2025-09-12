5719eb60fc8ee2f890e9da360758b7c9170a4a64
Jakob Runge <jakobrunge@gmail.com>
2025-09-12 21:45:15 +0200

    added warning in regressionCI about constant arrays

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ CMDCLASS = {"build_ext": UseNumpyHeadersBuildExt}
 # Run the setup
 setup(
     name="tigramite",
-    version="5.2.8.3",
+    version="5.2.8.4",
     packages=["tigramite", "tigramite.independence_tests", "tigramite.toymodels"],
     license="GNU General Public License v3.0",
     description="Tigramite causal inference for time series",
